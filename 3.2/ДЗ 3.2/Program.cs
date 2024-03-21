83e19d92434e07eb2d13c5644f8c64d31ac77e3a
Kseniya2308 <65559432+Kseniya2308@users.noreply.github.com>
2024-03-21 22:18:26 +0300

    Add files via upload

---
+++ 3.2/ДЗ 3.2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ДЗ_3._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+
+            Console.WriteLine("Добро пожаловать в игру 21! Подскажитие, сколько у вас карт?");
+            int cardsCount = byte.Parse(Console.ReadLine());
+            int score = 0;
+
+            for (int i = 1; i <= cardsCount; i++)
+            {
+                Console.WriteLine($"Введите номинал {i} карты:");
+                string card = (Console.ReadLine());
+
+
+                switch (card)
+                {
+                    case "J":
+                    case "Q":
+                    case "K":
+                    case "T":
+
+                        score += 10;
+
+                        break;
+                    default:
+
+                        score += int.Parse(card);
+                        break;
+                }
+
+
+            }
+
+
+            Console.WriteLine($"Вы набрали - {score}");
+
+
+        }
+    }
+
+
+}
