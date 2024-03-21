557555ab434f8d70f48029400ddba5d8bbd42c9c
Kseniya2308 <65559432+Kseniya2308@users.noreply.github.com>
2024-03-21 11:11:02 +0300

    Add files via upload

---
+++ 3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Design;
+
+namespace Домашняя_работа_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                
+            Console.WriteLine("Введите число:  ");
+
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} - четное число");
+            }
+            else 
+            { 
+                Console.WriteLine($"{number} - нечетное число"); 
+            }
+
+                
+        }
+    }
+}
