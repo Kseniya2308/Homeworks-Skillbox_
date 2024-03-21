32a1dad216ce97027fd7902a32721b59635a84c4
Kseniya2308 <65559432+Kseniya2308@users.noreply.github.com>
2024-03-21 22:19:27 +0300

    Add files via upload

---
+++ 3.4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _3._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину последовательности:");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int min = int.MaxValue;
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write($"Введите {i + 1}-е число: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+
+            Console.WriteLine($"Наименьшее число в последовательности: {min}");
+        }
+    }
+    }
+
