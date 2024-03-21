c7b5bb1279aaa6ae57da5b2e9cfc375fb386e2b0
Kseniya2308 <65559432+Kseniya2308@users.noreply.github.com>
2024-03-21 22:18:56 +0300

    Add files via upload

---
+++ 3.3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ДЗ_3._3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите целое число: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            bool result = true;
+
+            int i = 2;
+            while (i < N)
+            {
+                if (N % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+
+                i++;
+            }
+
+            if (result)
+            {
+                Console.WriteLine("Введенное число является простым.");
+            }
+            else
+            {
+                Console.WriteLine("Введенное число не является простым.");
+            }
+        }
+
+
+
+    }
+      
+        }
+
+
+
+            
+
+           
+        
+
+        
+       
+
+
+     
+    
+
+
